042719f0deab2e89d7694bfe509e96621203890d
Makerfabs <Lin@makerfabs.com>
2026-01-04 10:39:11 +0800

    delect example/esp-idf

--- example/esp-idf/lvgl9_demo/components/bsp/sdcard/bsp_sdcard.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <esp_err.h>
-#include <driver/gpio.h>
-
-
-esp_err_t bsp_sdcard_mount(const char *mount_point, gpio_num_t sd_cs);
--- example/esp-idf/sd_img/components/bsp/bsp_spiffs/bsp_spiffs.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <esp_err.h>
-
-
-esp_err_t bsp_spiffs_mount();
